18449f54883948f43219b21b4302dffb3d2fbcd4
romanlazko <romanlazko@gmail.com>
2023-11-25 16:38:25 +0100

    обновлени приложения

--- resources/views/components/web-app.blade.php
+++ resources/views/components/web-app.blade.php
@@ -1,21 +1,21 @@
 <div class="flex h-screen bg-gray-200 font-roboto ">
     <div class="relative flex-1 flex flex-col overflow-hidden">
         @if (isset($header))
-            <div class="bg-white  w-full">
-                <div class=" bg-white flex w-full m-auto h-[80px] fixed px-4 sm:px-6 lg:px-8 items-center justify-between max-w-2xl">
+            <div class="bg-white w-full h-[80px]">
+                <div class="bg-white flex w-full mx-auto h-[80px] px-4 sm:px-6 lg:px-8 items-center justify-between max-w-2xl">
                     {{ $header }}
                 </div>
             </div>
         @endif
         
-        <main class="my-[80px] flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+        <main class="flex-1 overflow-x-hidden w-full overflow-y-auto bg-gray-200">
             <div class="mx-auto max-w-2xl px-2">
                 {{$slot}}
             </div>
         </main>
 
         @if (isset($footer))
-            <div class="flex w-full m-auto items-center justify-between max-w-2xl space-x-4 h-[80px] fixed bottom-0 px-2">
+            <div class="flex w-full m-auto items-center justify-between max-w-2xl space-x-4 h-[80px] bottom-0 px-2">
                 {{ $footer }}
             </div>
         @endif
