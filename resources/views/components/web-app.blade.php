71cf06d0585c3ffac3a2d5fb6ebc5688882b92dc
romanlazko <romanlazko@gmail.com>
2023-11-25 17:25:44 +0100

    обновление приложения

--- resources/views/components/web-app.blade.php
+++ resources/views/components/web-app.blade.php
@@ -1,37 +1,63 @@
-<div class="bg-gray-200 font-roboto ">
-    <div class="overflow-hidden ">
+<div id="main" class="flex bg-gray-200 font-roboto ">
+    <div class="flex-1 flex flex-col overflow-hidden ">
         @if (isset($header))
-            <div class="bg-white fixed h-[10vh] w-full">
-                <div class="flex w-full h-[10vh] m-auto px-4 sm:px-6 lg:px-8 items-center justify-between max-w-2xl">
+            <div class="bg-white">
+                <div class="flex w-full m-auto px-4 sm:px-6 lg:px-8 min-h-[80px] items-center justify-between max-w-2xl">
                     {{ $header }}
                 </div>
                 <hr>
             </div>
         @endif
         
-        <main class="py-[10vh] flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
             <div class="mx-auto max-w-2xl">
                 {{$slot}}
             </div>
         </main>
 
         @if (isset($footer))
-            <div class="h-[10vh] fixed flex w-full m-auto items-center justify-between max-w-2xl space-x-4 bottom-0">
+            <div class="flex w-full m-auto items-center justify-between max-w-2xl space-x-4">
                 {{ $footer }}
             </div>
         @endif
     </div>
 </div>
+
+{{-- <div id="main" class="bg-gray-200 font-roboto ">
+    <div class="overflow-hidden ">
+        @if (isset($header))
+            <div class="bg-white fixed h-[10vh] w-full">
+                <div class="flex w-full h-[10vh] m-auto px-4 sm:px-6 lg:px-8 items-center justify-between max-w-2xl">
+                    {{ $header }}
+                </div>
+            </div>
+        @endif
+        
+        <main class="py-[12vh] px-2 flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+            <div class="mx-auto max-w-2xl">
+                {{$slot}}
+            </div>
+        </main>
+
+        @if (isset($footer))
+            <div class="bg-white fixed h-[10vh] w-full bottom-0 px-2">
+                <div class="h-[10vh] flex w-full mx-auto items-center justify-between max-w-2xl space-x-4 ">
+                    {{ $footer }}
+                </div>
+            </div>
+        @endif
+    </div>
+</div> --}}
 <script type="module">
     $(document).ready(function(){
     // Устанавливаем высоту блока равной высоте видимой области
-    var windowHeight = window.innerHeight;
-    $('main').css('height', windowHeight + 'px');
-    
-    // Обновляем высоту блока при изменении размеров окна браузера
-    $(window).resize(function(){
         var windowHeight = window.innerHeight;
-        $('main').css('height', windowHeight + 'px');
+        $('#main').css('height', windowHeight + 'px');
+        
+        // Обновляем высоту блока при изменении размеров окна браузера
+        $(window).resize(function(){
+            var windowHeight = window.innerHeight;
+            $('#main').css('height', windowHeight + 'px');
+        });
     });
-});
 </script>
