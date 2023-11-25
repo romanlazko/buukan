d7c2f813cd580fed47324f00d6256688aef5112d
romanlazko <romanlazko@gmail.com>
2023-11-25 17:18:01 +0100

    Обновление приложения

--- resources/views/components/web-app.blade.php
+++ resources/views/components/web-app.blade.php
@@ -1,36 +1,37 @@
-<div class="flex bg-gray-200 font-roboto overflow-hidden">
-    <div class="relative flex-1 flex flex-col overflow-hidden">
+<div class="bg-gray-200 font-roboto ">
+    <div class="overflow-hidden ">
         @if (isset($header))
-            <div class="bg-white w-full">
-                <div class="bg-white flex w-full mx-auto h-[10vh] fixed px-4 sm:px-6 lg:px-8 items-center justify-between max-w-2xl z-50">
+            <div class="bg-white fixed h-[10vh] w-full">
+                <div class="flex w-full h-[10vh] m-auto px-4 sm:px-6 lg:px-8 items-center justify-between max-w-2xl">
                     {{ $header }}
                 </div>
+                <hr>
             </div>
         @endif
         
-        <main class="my-[10vh] flex-1 overflow-x-hidden w-full overflow-y-auto bg-gray-200 fixed overflow-auto">
-            <div class="mx-auto max-w-2xl px-2">
+        <main class="py-[10vh] flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+            <div class="mx-auto max-w-2xl">
                 {{$slot}}
             </div>
         </main>
 
         @if (isset($footer))
-            <div class="flex w-full m-auto items-center justify-between max-w-2xl space-x-4 h-[10vh] fixed bottom-0 px-2">
+            <div class="h-[10vh] fixed flex w-full m-auto items-center justify-between max-w-2xl space-x-4 bottom-0">
                 {{ $footer }}
             </div>
         @endif
     </div>
 </div>
-<script>
-    $(document).ready(function() {
-    // Устанавливаем высоту блока равной высоте видимой области страницы
-        var windowHeight = $(window).height();
-        $('main').height(windowHeight);
+<script type="module">
+    $(document).ready(function(){
+    // Устанавливаем высоту блока равной высоте видимой области
+    var windowHeight = window.innerHeight;
+    $('main').css('height', windowHeight + 'px');
+    
+    // Обновляем высоту блока при изменении размеров окна браузера
+    $(window).resize(function(){
+        var windowHeight = window.innerHeight;
+        $('main').css('height', windowHeight + 'px');
     });
-
-    // Обновляем высоту блока при изменении размеров окна
-    $(window).resize(function() {
-        var windowHeight = $(window).height();
-        $('main').height(windowHeight);
-    });
+});
 </script>
